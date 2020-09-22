fff186af9fe6aa865f51837819f976e1ea1c9c72
sclurb <sclurb@outlook.com>
2020-09-22 09:36:36 -0400

    Completed up to module-4

---
+++ Queries/JustAClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Queries
+{
+    class JustAClass : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            
+        }
+    }
+}
